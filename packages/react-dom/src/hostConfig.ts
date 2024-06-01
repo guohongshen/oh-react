2ff509282daa2e7a225154cc0b81e55362053339
guohongshen <guohongshen2018@utlook.com>
2024-06-01 13:36:56 +0800

    feat: update 流程 commit 阶段

--- packages/react-dom/src/hostConfig.ts
+++ packages/react-dom/src/hostConfig.ts
@@ -1,5 +1,9 @@
+import { FiberNode } from "react-reconciler/src/fiber";
+import { HostText } from "react-reconciler/src/workTags";
+
 export type Container = Element;
 export type Instance = Element;
+export type textInstance = Text;
 
 export function createInstance (type: string, props?: any): Instance {
     // TODO 处理 props
@@ -17,4 +21,28 @@ export function createTextInstance(content: string) {
 
 export function appendChildToContainer(child: any, container: Container) {
     return appendInitialChild(container, child);
+}
+
+export function commitUpdate(fiber: FiberNode) {
+    switch (fiber.tag) {
+        case HostText:
+            const text = fiber.memoizedProps.content;
+            return commitTextUpdate(fiber.stateNode, text);
+        default:
+            if (__DEV__) {
+                console.warn('未实现的 Update 类型, fiber');
+            }
+            break;
+    }
+}
+
+export function commitTextUpdate(textInstance: textInstance, content: string) {
+    textInstance.textContent = content;
+}
+
+export function removeChild(
+    child: Instance | textInstance,
+    container: Container
+) {
+    container.removeChild(child);
 }
--- packages/react-reconciler/src/commitWork.ts
+++ packages/react-reconciler/src/commitWork.ts
@@ -1,7 +1,8 @@
-import { Container, appendChildToContainer } from "hostConfig";
+import { Container, appendChildToContainer, commitUpdate, removeChild } from "hostConfig";
 import { FiberNode, FiberRootNode } from "./fiber";
-import { MutationMask, NoFlags, Placement } from "./fiberFlags";
-import { HostComponent, HostRoot, HostText } from "./workTags";
+import { ChildDeletion, MutationMask, NoFlags, Placement, Update } from "./fiberFlags";
+import { FunctionComponent, HostComponent, HostRoot, HostText } from "./workTags";
+import { ExecFileOptionsWithBufferEncoding } from "child_process";
 
 let nextEffect: FiberNode | null = null;
 
@@ -36,8 +37,21 @@ function commitMutationEffectsOnFiber(finishedWork: FiberNode) {
 
         finishedWork.flags &= ~Placement; // 移除 Placement
     }
-    // TODO: flags Update
-    // TODO: flags ChildDeletion
+    if ((flags & Update) !== NoFlags) {
+        commitUpdate(finishedWork);
+
+        finishedWork.flags &= ~Update; // 移除 Update
+    }
+    if ((flags & ChildDeletion) !== NoFlags) {
+        const deletions = finishedWork.deletions;
+        if (deletions !== null) {
+            deletions.forEach(child => {
+                commitDeletion(child);
+            });
+        }
+        
+        finishedWork.flags &= ~ChildDeletion; // 移除 ChildDeletion
+    }
 }
 
 function commitPlacement(finishedWork: FiberNode) {
@@ -50,6 +64,101 @@ function commitPlacement(finishedWork: FiberNode) {
     // finishedWork ~ DOM
     if (hostParent !== null) {
         appendPlacementNodeIntoContainer(finishedWork, hostParent);
+    }
+}
+
+/**
+ * 也即：移除以 childToDelete 为根节点的子树。由于子树中：
+ * <1> 对于 FC，需要处理 useEffect unmount 执行、解绑 ref；
+ * <2> 对于 HostComponent，需要解绑 ref；
+ * <3> 对于子树中所有以 HostComponent 为根的子树，需要移除其根的 DOM 节点；
+ * 那么本函数(commitDeletion)一定是递归的，递归的遍历这棵子树。
+ * @param childToDelete 
+ */
+function commitDeletion(childToDelete: FiberNode) {
+    let rootHostNode: FiberNode | null = null;
+    // 递归子树
+    commitNestedComponent(childToDelete, unmountFiber => {
+        switch (unmountFiber.tag) {
+            case HostComponent:
+                if (rootHostNode === null) {
+                    rootHostNode = unmountFiber; // QUESTION
+                }
+                // TODO 解绑 ref
+                return;
+            case HostText:
+                if (rootHostNode === null) {
+                    rootHostNode = unmountFiber; // QUESTION
+                }
+                return;
+            case FunctionComponent:
+                // TODO useEffect unmount:
+                return;
+            default:
+                if (__DEV__) {
+                    console.warn('未处理的 unmount 类型');
+                }
+                break;
+        }
+    });
+
+    // 移除 rootHostNode 的 DOM
+    if (rootHostNode !== null) {
+        const hostParent = getHostParent(childToDelete);
+        // 单一节点，只考虑有一个子树的情况
+        (hostParent !== null) && removeChild(rootHostNode, hostParent);
+    }
+    childToDelete.return = null;
+    childToDelete.child = null;
+}
+
+function commitNestedComponent(
+    root: FiberNode,
+    onCommitUnmount: (fiber: FiberNode) => void
+) {
+    /*let node = root;
+    while (true) {
+        onCommitUnmount(node);
+
+        if (node.child !== null) {
+            node.child.return = node;
+            node = node.child;
+            continue;
+        }
+        if (node === root) {
+            return;
+        }
+        while (node.sibling === null) {
+            if (node.return === null || node.return === root) {
+                return;
+            }
+            node = node.return;
+        }
+        node.sibling.return = node.return;
+        node = node.sibling;
+    }*/
+    let node = root;
+    function toLeave(n: FiberNode) {
+        while (n.child !== null) {
+            n = n.child;
+        }
+        return n;
+    };
+    node = toLeave(node);
+    // node 现在是叶子节点
+    while (true) {
+        onCommitUnmount(node);
+        if (node === root) return;
+        if (node.sibling !== null) {
+            node = toLeave(node.sibling);
+            continue;
+        } else {
+            if (node.return === null) { // 应付类型检查
+                return;
+            } else {
+                node = node.return;
+            }
+        }
     }
 }
 
