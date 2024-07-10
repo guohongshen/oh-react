673acd320c9a6621dae7c43e7c60e804d813ba2e
guohongshen <guohongshen2018@utlook.com>
2024-07-10 15:52:01 +0800

    feat: 测试 useeffect, jest-react, scheduler/unstable_mock 报错

--- scripts/jest/jest.config.js
+++ scripts/jest/jest.config.js
@@ -1,16 +1,20 @@
 const { defaults } = require('jest-config');
 
-// 提示：jest 会自动读取项目目录下的 babel.config.js
-
 module.exports = {
     ...defaults,
     rootDir: process.cwd(),
     modulePathIgnorePatterns: ['<rootDir>/.history'],
     moduleDirectories: [
-        // 对于 React、ReactDOM
-        'dist/node_modules',
-        // 对于第三方依赖
-        ...defaults.moduleDirectories
+        ...defaults.moduleDirectories,
+        'dist/node_modules'
     ],
-    testEnvironment: 'jsdom' // 测试环境
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '^scheduler$': '<rootDir>/packages/scheduler/index.js'
+    },
+    fakeTimers: {
+        enableGlobally: true,
+        legacyFakeTimers: true
+    },
+    setupFilesAfterEnv: ['./scripts/jest/setupJest.js']
 };
