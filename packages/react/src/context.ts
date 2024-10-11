338304fe84374fa78a096a72c715592f1fa4ba7b
guohongshen <guohongshen2018@utlook.com>
2024-10-11 19:49:59 +0800

    feat: context 的创建

---
+++ packages/react/src/context.ts
@@ -0,0 +1,15 @@
+import { REACT_PROVIDER_TYPE, REACT_CONTEXT_TYPE } from "shared/ReactSymbols";
+import { ReactContext } from "shared/ReactTypes";
+
+export function createContext<T>(defaultValue: T): ReactContext<T> {
+    const context: ReactContext<T> = {
+        $$typeof: REACT_CONTEXT_TYPE,
+        Provider: null,
+        _currentValue: defaultValue
+    };
+    context.Provider = {
+        $$typeof: REACT_PROVIDER_TYPE,
+        _context: context
+    };
+    return context;
+}
